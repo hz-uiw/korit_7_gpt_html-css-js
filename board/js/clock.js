216540ca98ef26a499f8b597ae90ec62cf4339b8
hz-uiw <kwonmc511@gmail.com>
2025-01-02 12:46:45 +0900

    iphone frame - board

--- board/js/clock.js
+++ board/js/clock.js
@@ -3,7 +3,7 @@ function clockRun() {
     setInterval(() => {
         const now = new Date();
         const nowHours = now.getHours();
-        const nowMinutes = now.getMinutes();
+        const nowMinutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
         const nowClockText = `${nowHours}:${nowMinutes}`;
         phoneClock.innerHTML = nowClockText;
     }, 1000);
